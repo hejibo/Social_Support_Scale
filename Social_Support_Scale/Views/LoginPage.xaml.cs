8061636fb131fb6499238e7410cc59a12c4490b6
Jibo He <hejibo@live.com>
2017-05-30 14:03:19 -0500

    initial commit for social support scale

---
+++ Social_Support_Scale/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Social_Support_Scale
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
